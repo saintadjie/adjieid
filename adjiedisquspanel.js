e79f67c0e9d004aa59236a77d81321a481734ac7
saintadjie <saintadjie@gmail.com>
2021-09-27 13:01:15 +0700

    Update adjiedisquspanel.js

--- adjiedisquspanel.js
+++ adjiedisquspanel.js
@@ -4,4 +4,4 @@ $(document).ready(function(){$("#flippy").click(function(){$("#flippanel").slide
 setTimeout(function(){$(".video-youtube").each(function(){$(this).replaceWith('<iframe class="video-youtube loader" src="'+$(this).data("src")+'" allowfullscreen="allowfullscreen" height="281" width="500"></iframe>')})},5e3);
 // Disqus One
 var disqus_shortname = &quot;saintadjie&quot;;
-!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="//"+disqus_shortnamess+".disqus.com/blogger_item.js",(document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(e)}();
+!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="//"+disqus_shortname+".disqus.com/blogger_item.js",(document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(e)}();
